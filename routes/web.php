5fe27feb1388aaab0ea3ab55dc6a0a686012654d
Hoang Phi <philehoang99@gmail.com>
2021-01-03 22:59:20 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
 
 Auth::routes();
 
