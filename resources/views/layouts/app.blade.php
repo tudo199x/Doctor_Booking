58ac77154797b28c5be66b76dd19fb1daf1e7635
Hoang Phi <philehoang99@gmail.com>
2021-01-14 20:07:22 +0700

    layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"defer></script>
+
+   <!--for datepicker-->
+      <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+       <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"defer></script>
+      
+    <!--for datepicker-->
+
+        
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,12 +26,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+ <link rel="stylesheet" href="{{asset('template/dist/css/theme.min.css')}}">
+  
+
+
+   <!--for datepicker-->
+     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+   <!--for datepicker-->
+
+        
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a style="color: #fff;font-weight: bold;" class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -38,30 +55,45 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        @if(auth()->check()&& auth()->user()->role->name === 'patient')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('my.booking') }}" style="color: #fff; font-size:16px; font-weight: bold;">{{ __('Cuộc Hẹn Của Tôi') }}</a>
+                            </li>
+                        @endif
+                        @if(auth()->check()&& auth()->user()->role->name === 'patient')
+                            <li class="nav-item">
+                                <a style="color: #fff; font-size:16px; font-weight: bold;" class="nav-link" href="{{ route('my.prescription') }}" style="color: #fff; font-size:16px; font-weight: bold;">{{ __('Đơn Thuốc Của Tôi') }}</a>
+                            </li>
+                        @endif
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a style="color: #fff; font-size:16px; font-weight: bold;"  class="nav-link" href="{{ route('login') }}">{{ __('Đăng Nhập') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a style="color: #fff; font-size:16px; font-weight: bold;" class="nav-link" href="{{ route('register') }}" style="color: #fff; font-size:16px; font-weight: bold;">{{ __('Đăng Kí Là Bệnh Nhân') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                <a style="color: #fff; font-size:16px; font-weight: bold;" id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                     @if(auth()->check()&& auth()->user()->role->name === 'patient')
+                                    <a href="{{url('user-profile')}}"  class="dropdown-item"style="color: #000; font-size:16px; font-weight: bold;">Hồ Sơ</a>
+                                    @else 
+                                     <a href="{{url('dashboard')}}"  class="dropdown-item">Trang Chính</a>
+                                    @endif
+                                    <a style="color: #000; font-size:16px; font-weight: bold;" class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
@@ -73,8 +105,55 @@
         </nav>
 
         <main class="py-4">
+
             @yield('content')
         </main>
     </div>
+        
+
+
+ <script>
+    var dateToday = new Date();
+  $( function() {
+    $("#datepicker").datepicker({
+        dateFormat:"yy-mm-dd",
+        showButtonPanel:true,
+        numberOfMonths:2,
+        minDate:dateToday,
+    });
+});
+
+  </script>
+<style type="text/css">
+    body{
+        background: #fff;
+    }
+    .ui-corner-all{
+        background: red;
+        color: #fff;
+    }
+    label.btn{
+        padding: 0;
+    }
+    label.btn input{
+        opacity: 0; 
+        position: absolute;
+    }
+    label.btn span{
+        text-align: center; 
+        padding: 6px 12px; 
+        display: block;
+        min-width: 80px;
+    }
+    label.btn input:checked+span{
+        background-color: rgb(80,110,228); 
+        color: #fff;
+    }
+    .navbar{
+        background:#6610f2!important;
+        color: #fff!important;
+    }
+</style>
+  
 </body>
 </html>
